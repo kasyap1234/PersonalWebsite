a8e3625e3f75827c9ee527e2101756dd078ad9d6
kasyap dharanikota <76957823+kasyap1234@users.noreply.github.com>
2024-05-01 13:14:25 +0530

    buttons added

--- personalwebsite/app/landing.tsx
+++ personalwebsite/app/landing.tsx
@@ -5,10 +5,11 @@ export default function Landing() {
             <div className="flex flex-col items-top">
         
         <div className="flex space-x-4 mt-8 ">
-          <button  className="text-white bg-black">LinkedIn</button>
-          <button  className="text-white bg-black ">Substack</button>
-          <button  className="text-white bg-black">Twitter</button>
-          <button  className="text-white bg-black">GitHub</button>
+          <button  className="text-white bg-black px-3 py-1.5 text-base rounded-md hover:bg-opacity-80">LinkedIn</button>
+  
+          <button  className="text-white bg-black px-3 py-1.5 text-base   rounded-md hover:bg-opacity-80">Substack</button>
+          <button  className="text-white bg-black px-3 py-1.5 text-base  rounded-md hover:bg-opacity-80">Twitter</button>
+          <button  className="text-white bg-black px-3 py-1.5 text-base   rounded-md hover:bg-opacity-80">GitHub</button>
         </div>
       </div>
             </main> 
